bb08154e3de865dbc7c8fc67a975455aee7c05db
Evan Zhang <ezhang366@gmail.com>
2021-01-30 15:37:41 -0500

    i can grab things now

--- CandysMansion/Assets/Scripts/PlayerController.cs
+++ CandysMansion/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using Luminosity.IO;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController singleton;
-    public GameObject camera;
+    public GameObject cameraAnchor;
     public float speed;
     public float cameraSensitivity;
     Rigidbody rb;
@@ -25,16 +25,18 @@ public class PlayerController : MonoBehaviour
         float camx = InputManager.GetAxis("CameraH");
         float camy = InputManager.GetAxis("CameraV");
         transform.Rotate(0f, camx * cameraSensitivity, 0f);
-        camera.transform.Rotate(new Vector3(0f, camx * cameraSensitivity, 0f), Space.World);
-        camera.transform.Rotate(new Vector3(camy * cameraSensitivity, 0f, 0f), Space.Self);
+        cameraAnchor.transform.Rotate(new Vector3(0f, camx * cameraSensitivity, 0f), Space.World);
+        cameraAnchor.transform.Rotate(new Vector3(camy * cameraSensitivity, 0f, 0f), Space.Self);
 
         //move player (and camera anchor)
         float x = InputManager.GetAxis("Horizontal");
         float z = InputManager.GetAxis("Vertical");
         rb.AddForce(transform.rotation*(new Vector3(x, 0f, z)) * speed);
-        camera.transform.position = transform.position;
+        cameraAnchor.transform.position = transform.position;
 
         bool leftGrabbing = InputManager.GetAxis("Grab") <= -0.5f;
         bool rightGrabbing = InputManager.GetAxis("Grab") >= 0.5f;
+
+
     }
 }
