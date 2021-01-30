b9f83a8c2fe99f62a1397f2c2cc7feb567237ef4
Dhaval Patel <dhavalppatel@users.noreply.github.com>
2021-01-30 14:47:24 -0500

    Chute + More Prefabs

---
+++ CandysMansion/Assets/Scripts/BoxChute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxChute : MonoBehaviour
+{
+    public bool isActive;
+    public GameObject spawnPoint;
+    public GameObject boxToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(boxToSpawn != null)
+        {
+            Instantiate(boxToSpawn, spawnPoint.transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
