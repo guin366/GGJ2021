ec92efc60200e2967a550fa694116fdbc9b3e593
bjornthefire <bjorntalbot@gmail.com>
2021-01-30 15:07:15 -0800

    added level timer

---
+++ CandysMansion/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour
+{
+    public float currentTime;
+    public GameObject trigger;
+    private bool active;
+
+    void Update()
+    {
+        if(active)
+        {
+            if(currentTime > 0)
+            {
+               currentTime -= Time.deltaTime; 
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(active)
+        {
+            active = false;
+            //check if box is held by player
+            //win
+        }
+        else
+        {
+            active = true;
+        }
+    }
+}
