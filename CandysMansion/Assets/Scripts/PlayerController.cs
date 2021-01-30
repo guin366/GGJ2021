1ba5cf38097be106e30ad1da904fb1c0647ccd99
Evan Zhang <ezhang366@gmail.com>
2021-01-30 17:44:53 -0500

    jump and throws implemented

--- CandysMansion/Assets/Scripts/PlayerController.cs
+++ CandysMansion/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,10 @@ public class PlayerController : MonoBehaviour
     public GameObject cameraAnchor;
     public float speed;
     public float cameraSensitivity;
+    public Vector2 drag = new Vector2(10f, 0f);
     Rigidbody rb;
+    public float jumpVel;
+    public float gravity = 20f;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,9 +21,32 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void OnEnable()
+    {
+        cameraAnchor.transform.rotation = Quaternion.identity;
+        cameraAnchor.transform.Rotate(0f, transform.rotation.y, 0f, Space.World);
+    }
+
+    private void Update()
+    {
+        //perform jump if on ground
+        if (InputManager.GetButtonDown("Jump") && OnGround())
+        {
+            print("jump?");
+            rb.velocity = new Vector3(rb.velocity.x, jumpVel, rb.velocity.z);
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
+        //manual drag implementation
+        Vector3 newv = rb.velocity;
+        newv.x *= 1 - Time.fixedDeltaTime * drag.x;
+        newv.y *= 1 - Time.fixedDeltaTime * drag.y;
+        newv.z *= 1 - Time.fixedDeltaTime * drag.x;
+        rb.velocity = newv;
+
         //manipulate camera
         float camx = InputManager.GetAxis("CameraH");
         float camy = InputManager.GetAxis("CameraV");
@@ -29,17 +55,19 @@ public class PlayerController : MonoBehaviour
         if (!((Mathf.DeltaAngle(0f, Camera.main.transform.rotation.eulerAngles.x) >= 75 && camy > 0f) || 
             (Mathf.DeltaAngle(0f,Camera.main.transform.rotation.eulerAngles.x) <= -75f && camy < 0f)))
             cameraAnchor.transform.Rotate(new Vector3(camy * cameraSensitivity, 0f, 0f), Space.Self);
-        
 
         //move player (and camera anchor)
         float x = InputManager.GetAxis("Horizontal");
         float z = InputManager.GetAxis("Vertical");
-        rb.AddForce(transform.rotation*(new Vector3(x, 0f, z)) * speed);
+        rb.AddForce(transform.rotation * (new Vector3(x, 0f, z)) * speed);
         cameraAnchor.transform.position = transform.position;
 
-        bool leftGrabbing = InputManager.GetAxis("Grab") <= -0.5f;
-        bool rightGrabbing = InputManager.GetAxis("Grab") >= 0.5f;
+        //apply gravity
+        rb.AddForce(Vector3.down * gravity * 60f * Time.fixedDeltaTime);
+    }
 
-
+    bool OnGround()
+    {
+        return true;
     }
 }
