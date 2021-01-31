6f0a6155dd54d2ef0d5e24051bb80cb7fc1b55b4
Dhaval Patel <dhavalppatel@users.noreply.github.com>
2021-01-31 17:18:53 -0500

    postoffice3 level loading

---
+++ CandysMansion/Assets/Scripts/QuitGameButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class QuitGameButton : MonoBehaviour
+{
+    public Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quitButton.onClick.AddListener(quitButtonPressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void quitButtonPressed()
+    {
+        
+        print("CLICKED!!!");
+        Loader.Load(Loader.Scene.PostOffice1);
+   
+    }
+}
